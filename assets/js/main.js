d8885c25222c3a3434fc58125c83dcf4b735fb22
Mountaga <baldemtaga@gmail.com>
2024-05-26 01:25:19 +0000

    Updated comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,6 @@
-/**
-* Template Name: Personal
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -21,9 +10,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
 
@@ -36,9 +22,6 @@
     }
   }
 
-  /**
-   * Scrolls to an element with header offset
-   */
   const scrollto = (el) => {
     window.scrollTo({
       top: 0,
@@ -46,18 +29,12 @@
     })
   }
 
-  /**
-   * Mobile nav toggle
-   */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
   on('click', '#navbar .nav-link', function(e) {
     let section = select(this.hash)
     if (section) {
@@ -109,9 +86,6 @@
     }
   }, true)
 
-  /**
-   * Activate/show sections on load with hash links
-   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       let initial_nav = select(window.location.hash)
@@ -139,9 +113,6 @@
     }
   });
 
-  /**
-   * Skills animation
-   */
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -156,9 +127,6 @@
     })
   }
 
-  /**
-   * Testimonials slider
-   */
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -185,9 +153,6 @@
     }
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -213,25 +178,16 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Initiate portfolio details lightbox 
-   */
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
 
-  /**
-   * Portfolio details slider
-   */
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -246,9 +202,6 @@
     }
   });
 
-  /**
-   * Initiate Pure Counter 
-   */
   new PureCounter();
 
 })()
